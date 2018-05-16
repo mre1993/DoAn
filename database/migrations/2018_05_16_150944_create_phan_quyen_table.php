4378abbc638e18a7d225a2ff70695c5cbbfd4af6
mre1993 <buiquangloi1993@gmail.com>
2018-05-16 23:10:21 +0700

    push

---
+++ database/migrations/2018_05_16_150944_create_phan_quyen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhanQuyenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phan_quyen', function (Blueprint $table) {
+            $table->increments('Id');
+            $table->string('TenQuyen')-<d;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phan_quyen');
+    }
+}
