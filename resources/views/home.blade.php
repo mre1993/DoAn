0c8ba188953a137a35938f0b18d5b8c1e827602a
mre1993 <buiquangloi1993@gmail.com>
2018-05-19 17:17:12 +0700

    fix link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,35 +4,13 @@
     <ul>
         <li class="active"><a href="/home"><i class="fa fa-home"></i> <span>Dashboard</span></a> </li>
         <li> <a href="/user"><i class="fa fa-user-circle"></i> <span>Tài Khoản</span></a> </li>
-        <li> <a href="widgets.html"><i class="fa fa-inbox"></i> <span>Widgets</span></a> </li>
-        <li><a href="tables.html"><i class="fa fa-th"></i> <span>Tables</span></a></li>
-        <li><a href="grid.html"><i class="fa fa-fullscreen"></i> <span>Full width</span></a></li>
-        <li class="submenu"> <a href="#"><i class="fa fa-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
-            <ul>
-                <li><a href="form-common.html">Basic Form</a></li>
-                <li><a href="form-validation.html">Form with Validation</a></li>
-                <li><a href="form-wizard.html">Form with Wizard</a></li>
-            </ul>
-        </li>
-        <li><a href="buttons.html"><i class="fa fa-tint"></i> <span>Buttons &amp; icons</span></a></li>
-        <li><a href="interface.html"><i class="fa fa-pencil"></i> <span>Eelements</span></a></li>
-        <li class="submenu"> <a href="#"><i class="fa fa-file"></i> <span>Addons</span> <span class="label label-important">5</span></a>
-            <ul>
-                <li><a href="index2.html">Dashboard2</a></li>
-                <li><a href="gallery.html">Gallery</a></li>
-                <li><a href="calendar.html">Calendar</a></li>
-                <li><a href="invoice.html">Invoice</a></li>
-                <li><a href="chat.html">Chat option</a></li>
-            </ul>
-        </li>
-        <li class="submenu"> <a href="#"><i class="fa fa-info-sign"></i> <span>Error</span> <span class="label label-important">4</span></a>
-            <ul>
-                <li><a href="error403.html">Error 403</a></li>
-                <li><a href="error404.html">Error 404</a></li>
-                <li><a href="error405.html">Error 405</a></li>
-                <li><a href="error500.html">Error 500</a></li>
-            </ul>
-        </li>
+        <li> <a href="/provider"><i class="fa fa-inbox"></i> <span>Nhà cung cấp</span></a></li>
+        <li><a href="/factories"><i class="fa fa-th"></i> <span>Phân xưởng</span></a></li>
+        <li><a href="/material"><i class="fa fa-th"></i> <span>Vật tư</span></a></li>
+        <li><a href="/factories"><i class="fa fa-th"></i> <span>Loại vật tư</span></a></li>
+        <li><a href="/factories"><i class="fa fa-th"></i> <span>Kho vật tư</span></a></li>
+        <li><a href="/factories"><i class="fa fa-th"></i> <span>Nhân viên</span></a></li>
+        <li><a href="/factories"><i class="fa fa-th"></i> <span>Kiểm tra</span></a></li>
     </ul>
 </div>
 @yield('right-content')
