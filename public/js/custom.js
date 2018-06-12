8c045c85fd1d4581e5a25e9e47e62fd60cbb157f
mre1993 <buiquangloi1993@gmail.com>
2018-06-13 03:08:19 +0700

    modify

--- public/js/custom.js
+++ public/js/custom.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
     function drawChart() {
         var data1 = google.visualization.arrayToDataTable(jsonData1);
         // Optional; add a title and set the width and height of the chart
-        var options = {'title':'Nhà cung cấp lớn nhất', 'width':550, 'height':400};
+        var options = {'title':'Vật tư nhập nhiều nhất', 'width':550, 'height':400};
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.PieChart(document.getElementById('piechart-1'));
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,6 +38,11 @@
 </div>
 
 @yield('right-content')
-<div id="piechart-1"></div>
-<div id="piechart-2"></div>
+<div class="right-content col-md-12">
+    <div class="col-md-12">
+        <h2>Dashboard</h2>
+    </div>
+    <div id="piechart-1"></div>
+    <div id="piechart-2"></div>
+</div>
 @stop
