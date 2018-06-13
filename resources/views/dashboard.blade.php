b14edba506494d777918792b602e7bd7a8ee91ca
mre1993 <buiquangloi1993@gmail.com>
2018-06-14 00:53:11 +0700

    add homepage

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('home')
+@section('right-content')
+    <div class="right-content col-md-12">
+        <div class="col-md-12">
+            <h2>Dashboard</h2>
+        </div>
+        <div id="piechart-1"></div>
+        <div id="piechart-2"></div>
+    </div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,11 +53,4 @@
 </div>
 
 @yield('right-content')
-<div class="right-content col-md-12">
-    <div class="col-md-12">
-        <h2>Dashboard</h2>
-    </div>
-    <div id="piechart-1"></div>
-    <div id="piechart-2"></div>
-</div>
 @stop
